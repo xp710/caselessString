b575bc7b9595ad6deab080fa9347c19596fe50c4
xp710 <66922387+xp710@users.noreply.github.com>
2020-11-10 09:31:37 -0500

    Updated caselessCompare
    Made it more efficient by using string iterators instead of casing an int. Also compared those iterators one at a time instead of created two strings.

--- caselesscompare.cpp
+++ caselesscompare.cpp
@@ -1,6 +1,6 @@
 #include "xptools.h"
 
-bool xptools::caselessCompare(std::string* p_str1, std::string* p_str2){
+bool xptools::caselessCompare(std::string& p_str1, std::string& p_str2){
   /*
    * First, make sure the string lenths are equal.
    * If they aren't, the strings can't be equal, and we return there.
@@ -8,23 +8,21 @@ bool xptools::caselessCompare(std::string* p_str1, std::string* p_str2){
    * make lowercase versions of each string and test their equality
    */
 
-  if (p_str1->size() == p_str2->size()){
-    //Declare 2 temporary strings to be compared
-    std::string tmp1;
-    std::string tmp2;
-
-    //Assign lowercase versions of p_str1&2 to tmp1&2
-    for (int i = 0; static_cast<size_t>(i) < p_str1->size(); ++i){
-      tmp1 = std::tolower(p_str1->at(i));
-      tmp2 = std::tolower(p_str2->at(i));
-    }
-
-    //Compare tmp1&2 and return the results
-    return (tmp1 == tmp2);
-
-  }else{
-    //If they aren't equal in length, we know they aren't equal
-    //so we skip all the other stuff and return false
+  //Immediatly end the function if they aren't equal in length
+  if (p_str1.size() != p_str2.size()){
     return false;
   }
+
+  //If they are, compare each letter, converted to lowercase. As soon as one pair doesn't match, end the function
+  for (std::string::iterator l_it = p_str1.begin(), r_it = p_str2.begin();
+       l_it < p_str1.end(); ++l_it, ++r_it)
+  {
+    if (std::tolower(*l_it) != std::tolower(*r_it)){
+      return false;
+    }
+  }
+
+  //If it makes it this far, they're equal
+  return true;
 }
+
--- xptools.h
+++ xptools.h
@@ -8,7 +8,7 @@
 namespace xptools
 {
   //The function for comparing string without case
-  extern bool caselessCompare(std::string* p_str1, std::string* p_str2); //Take two string arguments and return their case-agnostic equality
+  extern bool caselessCompare(std::string& p_str1, std::string& p_str2); //Take two string arguments and return their case-agnostic equality
 }
 
 #endif //XPTOOLS_H
